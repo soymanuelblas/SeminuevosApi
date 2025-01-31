5a47a9ed3c898d103ad85ed7eaa2d1df04dd052f
roelleal <iroeleal11@gmail.com>
2025-01-30 20:31:56 -0600

    Refactor get_user method in UserModel.php

--- application/models/UserModel.php
+++ application/models/UserModel.php
@@ -4,22 +4,14 @@ class UserModel extends CI_Model {
 
     function get_user($id) {
         $id = (int)$id;
-        log_message('info', "Buscando usuario ID: $id");
-
-        $this->db->select('nombre'); // Siempre incluir el ID
+        $this->db->select('id, nombre, usr, tipostatus_id');
         $this->db->from('usuario');
         $this->db->where('id', $id);
         $query = $this->db->get();
-
-        log_message('info', "SQL ejecutado: " . $this->db->last_query());
-
         if($query->num_rows() > 0) {
             $result = $query->row_array();
-            log_message('info', "Usuario encontrado: " . print_r($result, true));
             return $result;
         }
-
-        log_message('error', "Usuario no encontrado para ID: $id");
         return false;
     }
 
